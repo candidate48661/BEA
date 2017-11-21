1d292feebd2999eb042da1f606c0fdc33103225f
Kirill Vostretsov <KVostretsov@gmail.com>
2017-11-21 15:59:40 +0100

    Update model script to support task database schema

--- api/models.py
+++ api/models.py
@@ -8,3 +8,18 @@ class MessageModel:
         self.message_category = message_category
         self.printed_times = 0
         self.printed_once = False
+
+
+class AccountModel:
+    def __init__(self, account_type, account_number, name, first_name, address, birthdate):
+        # We will automatically generate the new id
+        self.id = 0
+        self.type = account_type
+        self.number = account_number
+        self.name = name
+        self.first_name = first_name
+        self.address = address
+        self.birthdate = birthdate
+        #We will automatically generate next 2 parameters based on client address.
+        self.longitude = 0;
+        self.latitude = 0;
