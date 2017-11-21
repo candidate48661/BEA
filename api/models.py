8b1a8720a49468c67893d546d53b51ce5fda5055
Kirill Vostretsov <KVostretsov@gmail.com>
2017-11-21 20:09:34 +0100

    implement method to have several constructors for the class

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+import json
+
 class AccountModel:
     def __init__(self, account_type, account_number, name, first_name, address, birthdate):
         # We will automatically generate the new id
@@ -9,5 +11,22 @@ class AccountModel:
         self.address = address
         self.birthdate = birthdate
         #We will automatically generate next 2 parameters based on client address.
-        self.longitude = 0;
-        self.latitude = 0;
+        self.longitude = 0
+        self.latitude = 0
+
+    @classmethod
+    def fromData(self, entries):
+        id = entries["id"]
+        type = entries["type"]
+        number = entries["number"]
+        name = entries["name"]
+        first_name = entries["first_name"]
+        address = entries["address"]
+        birthdate = entries["birthdate"]
+        longitude = entries["longitude"]
+        latitude = entries["latitude"]
+        result = AccountModel(type, number, name, first_name, address, birthdate)
+        result.id = id
+        result.latitude = latitude
+        result.longitude = longitude
+        return result
