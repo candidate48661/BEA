0b024290a603fd2bcfb7d458a6610eff4a4a4dbe
Kirill Vostretsov <KVostretsov@gmail.com>
2017-11-21 11:54:50 +0100

    Message manager class implemented

--- api/api.py
+++ api/api.py
@@ -4,3 +4,22 @@ from datetime import datetime as dt
 from pytz import utc
 from models import MessageModel
 import status
+
+
+class MessageManager():
+    last_id = 0;
+    def __init__(self):
+        self.messages = {}
+
+    def insert_message(self, message):
+        self.__class__.last_id += 1
+        message.id = self.__class__.last_id
+        self.messages[self.__class__.last_id] = message
+
+    def get_message(self, id):
+        return self.messages[id]
+
+    def delete_message(self, id):
+        del self.messages[id]
+
+    
